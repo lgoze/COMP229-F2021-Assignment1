f06b29c74cc3570c98c50a22d8012e4503109fad
lgoze <lgoze@my.centennialcollege.ca>
2021-10-01 14:19:35 -0400

    Assignment1 Routing Partial

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,19 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const router = express_1.default.Router();
 router.get('/', function (req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Home' });
+});
+router.get('/about', function (req, res, next) {
+    res.render('index', { title: 'About Me' });
+});
+router.get('/projects', function (req, res, next) {
+    res.render('index', { title: 'Projects' });
+});
+router.get('/services', function (req, res, next) {
+    res.render('index', { title: 'Services' });
+});
+router.get('/contact', function (req, res, next) {
+    res.render('index', { title: 'Contact Me' });
 });
 exports.default = router;
 //# sourceMappingURL=index.js.map
